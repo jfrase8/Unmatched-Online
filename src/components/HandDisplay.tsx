b40b121371947c0f7608a7cea2ece0fa37211ffe
Joel Frase <37312935+jfrase8@users.noreply.github.com>
2025-03-06 21:59:47 -0600

    Card display finally looks good!

--- src/components/HandDisplay.tsx
+++ src/components/HandDisplay.tsx
@@ -1,12 +1,14 @@
+import clsx from 'clsx'
 import { Card } from '../constants/deckInfo'
+import { useState } from 'react'
 
 interface HandDisplayProps {
 	cards: Card[]
 }
 export default function HandDisplay({ cards }: HandDisplayProps) {
 	return (
-		<div className='relative flex justify-center items-center w-[40%] h-[30vh] overflow-x-auto bg-slate-400 p-2'>
-			<div className='flex gap-2 min-w-max'>
+		<div className='flex items-center max-w-[40%] h-[30vh] overflow-x-auto overflow-y-hidden bg-slate-400 transition-all duration-500'>
+			<div className='flex gap-2 min-w-max p-4 transition-all duration-500 h-full'>
 				{cards.map((card, i) => (
 					<CardDisplay key={i} {...card} index={i} cardsInHand={cards.length} />
 				))}
@@ -20,16 +22,40 @@ interface CardDisplayProps {
 	cardsInHand: number
 }
 export function CardDisplay({ name, imagePath, index, cardsInHand }: Card & CardDisplayProps) {
+	const [hovering, setHovering] = useState(false)
+
+	const getMargin = () => {
+		if (cardsInHand < 4) return '-ml-[1rem]'
+		switch (cardsInHand) {
+			case 4:
+				return '-ml-[2rem]'
+			case 5:
+				return '-ml-[3rem]'
+			case 6:
+				return '-ml-[4.2rem]'
+			case 7:
+				return '-ml-[5.4rem]'
+			case 8:
+				return '-ml-[6.3rem]'
+			case 9:
+				return '-ml-[7rem]'
+			case 10:
+				return '-ml-[7.5rem]'
+		}
+		if (cardsInHand > 10) return '-ml-[8rem]'
+	}
+
 	return (
 		<div
-			className='bg-gray-200 shadow-lg rounded-md transition-all duration-500 flex-shrink-0'
-			style={{
-				// Dynamically set width & height based on parent height
-				width: `calc(5vh + 5vw)`, // Scales with both height and width for consistency
-				height: `90%`, // Keeps the height proportional to HandDisplay
-			}}
+			className={clsx(
+				'shadow-lg rounded-md transition-all duration-500 border border-black h-full',
+				index !== 0 && getMargin(),
+				hovering && 'z-[100] scale-[110%]'
+			)}
+			onMouseEnter={() => setHovering(true)}
+			onMouseLeave={() => setHovering(false)}
 		>
-			<img src={imagePath} className='aspect-[] object-cover rounded-md' />
+			<img src={imagePath} className={clsx('aspect-[--card-aspect] size-full rounded-md')} />
 		</div>
 	)
 }
--- src/routes/play.lazy.tsx
+++ src/routes/play.lazy.tsx
@@ -12,7 +12,7 @@ function Play() {
 	return (
 		<div className='flex w-full h-[calc(100dvh-var(--navbar-height))] bg-gray-900 justify-center items-center'>
 			{/* <CreateJoinLobby /> */}
-			<HandDisplay cards={decks.Alice.cards.slice(0, 7)} />
+			<HandDisplay cards={[...decks.Alice.cards.slice(0, 3), ...decks.Alice.cards.slice(0, 1)]} />
 			{/* <CharacterSelection /> */}
 		</div>
 	)
